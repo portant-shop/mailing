da60918aa4e301f6d30d181ec4235ebebaf61ff4
Vedran Vojvoda <vedran@pinkdroids.com>
2022-02-24 18:10:47 +0100

    Initial commit

---
+++ makemigrations.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import os
+import sys
+
+import django
+
+from django.conf import settings
+
+
+DEFAULT_SETTINGS = dict(
+    DEBUG=True,
+    USE_TZ=True,
+    TIME_ZONE='UTC',
+    DATABASES={
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": ":memory:",
+        }
+    },
+    MIDDLEWARE_CLASSES=[
+        "django.contrib.sessions.middleware.SessionMiddleware",
+        "django.contrib.auth.middleware.AuthenticationMiddleware",
+        "django.contrib.messages.middleware.MessageMiddleware"
+    ],
+    INSTALLED_APPS=[
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.sites",
+        "mailing",
+    ],
+    EMAIL_FROM='mailing@example.com',
+    SITE_ID=1,
+)
+
+
+def run(*args):
+    """Do django setup and run makemigrations mgmt command."""
+    if not settings.configured:
+        settings.configure(**DEFAULT_SETTINGS)
+
+    django.setup()
+
+    parent = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, parent)
+
+    django.core.management.call_command(
+        "makemigrations",
+        "mailing",
+        *args
+    )
+
+
+if __name__ == "__main__":
+    run(*sys.argv[1:])
